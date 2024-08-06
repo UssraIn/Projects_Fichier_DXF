c5b848fe6d2e79ddeacf2d6f51f2317dbf836e99
UssraIn <164646785+UssraIn@users.noreply.github.com>
2024-08-06 22:43:39 +0200

    Pour Linge

---
+++ Project_Fichier_DXF/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Project_Fichier_DXF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(164, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(416, 449);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(682, 449);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pic.Location = new System.Drawing.Point(164, 205);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(655, 194);
+            this.pic.TabIndex = 3;
+            this.pic.TabStop = false;
+            this.pic.Paint += new System.Windows.Forms.PaintEventHandler(this.pic_Paint_1);
+            this.pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_MouseDown_1);
+            this.pic.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(164, 137);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(655, 51);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(651, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Exporte DXF...............................................................dxf  ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(710, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "By:YOUSRA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 534);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project_Fichier_DXF/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Fichier_DXF
+{
+    public partial class Form1 : Form
+    {
+        private List<Line> lines = new List<Line>();
+        private bool drawing = false;
+        private Point startPoint;
+        private string fileName = @"C:\Test.dxf";
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = fileName;
+            pic.MouseDown += new MouseEventHandler(pic_MouseDown_1);
+            pic.MouseMove += new MouseEventHandler(pic_MouseMove_1);
+            pic.MouseUp += new MouseEventHandler(pic_MouseUp);
+            pic.Paint += new PaintEventHandler(pic_Paint_1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "DXF files (*.dxf)|*.dxf",
+                Title = "Save DXF File",
+                FileName = textBox1.Text
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog.FileName;
+                ExportToDXF(fileName);
+                MessageBox.Show("Export completed.");
+            }
+        }
+            private void ExportToDXF(string fileName)
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    writer.WriteLine("0");
+                    writer.WriteLine("SECTION");
+                    writer.WriteLine("2");
+                    writer.WriteLine("HEADER");
+                    writer.WriteLine("0");
+                    writer.WriteLine("ENDSEC");
+                    writer.WriteLine("0");
+                    writer.WriteLine("SECTION");
+                    writer.WriteLine("2");
+                    writer.WriteLine("TABLES");
+                    writer.WriteLine("0");
+                    writer.WriteLine("TABLE");
+                    writer.WriteLine("2");
+                    writer.WriteLine("LAYER");
+                    writer.WriteLine("70");
+                    writer.WriteLine("1");
+                    writer.WriteLine("0");
+                    writer.WriteLine("LAYER");
+                    writer.WriteLine("2");
+                    writer.WriteLine("Layer1");
+                    writer.WriteLine("70");
+                    writer.WriteLine("0");
+                    writer.WriteLine("62");
+                    writer.WriteLine("7");
+                    writer.WriteLine("6");
+                    writer.WriteLine("CONTINUOUS");
+                    writer.WriteLine("0");
+
+                    writer.WriteLine("ENDTAB");
+                    writer.WriteLine("0");
+                    writer.WriteLine("ENDSEC");
+                    writer.WriteLine("0");
+                    writer.WriteLine("SECTION");
+                    writer.WriteLine("2");
+                    writer.WriteLine("ENTITIES");
+
+                    foreach (var line in lines)
+                    {
+                        writer.WriteLine("0");
+                        writer.WriteLine("LINE");
+                        writer.WriteLine("8");
+                        writer.WriteLine("Layer1");
+                        writer.WriteLine("10");
+                        writer.WriteLine(line.Start.X);
+                        writer.WriteLine("20");
+                        writer.WriteLine(pic.Height - line.Start.Y);
+                        writer.WriteLine("30");
+                        writer.WriteLine("0");
+                        writer.WriteLine("11");
+                        writer.WriteLine(line.End.X);
+                        writer.WriteLine("21");
+                        writer.WriteLine(pic.Height - line.End.Y);
+                        writer.WriteLine("31");
+                        writer.WriteLine("0");
+                    }
+
+                    writer.WriteLine("0");
+                    writer.WriteLine("ENDSEC");
+                    writer.WriteLine("0");
+                    writer.WriteLine("EOF");
+                }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            lines.Clear();
+            pic.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit  ?", "Exit confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void pic_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                startPoint = e.Location;
+                drawing = true;
+            }
+            else if (e.Button == MouseButtons.Right && lines.Count > 0)
+            {
+                lines.RemoveAt(lines.Count - 1);
+                pic.Invalidate();
+            }
+        }
+
+        private void pic_MouseMove_1(object sender, MouseEventArgs e)
+        {
+
+
+        }
+
+        private void pic_MouseUp(object sender, MouseEventArgs e)
+        { 
+                if (drawing)
+                {
+                    drawing = false;
+                    lines.Add(new Line(startPoint, e.Location));
+                    pic.Invalidate();
+                };
+        }
+
+        private void pic_Paint_1(object sender, PaintEventArgs e)
+        { 
+                foreach (var line in lines)
+                {
+                    e.Graphics.DrawLine(Pens.Black, line.Start, line.End);
+                }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Line
+        {
+            public Point Start { get; set; }
+            public Point End { get; set; }
+
+            public Line(Point start, Point end)
+            {
+                Start = start;
+                End = end;
+            }
+        }
+}
